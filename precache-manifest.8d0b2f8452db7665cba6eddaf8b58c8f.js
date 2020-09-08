7e1d17c4022211a980a5148d524cd00914bfa6ff
wellnemo <wbpochta@mail.ru>
2020-09-08 12:16:40 +0300

    Updates

---
+++ precache-manifest.8d0b2f8452db7665cba6eddaf8b58c8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1335fa82ab5ea16c3467886360bfc385",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "2b2698c4276c4541b432",
+    "url": "/new/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9dac5fc3fa13bac28d1f",
+    "url": "/new/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2b2698c4276c4541b432",
+    "url": "/new/static/js/2.b7d865f1.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/new/static/js/2.b7d865f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dac5fc3fa13bac28d1f",
+    "url": "/new/static/js/main.48bd6b37.chunk.js"
+  },
+  {
+    "revision": "278aee4cc6044938feaa",
+    "url": "/new/static/js/runtime-main.5681dd28.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/new/static/media/persik.4e1ec840.png"
+  }
+]);
